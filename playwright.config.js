aa57679cb2e5297371734253a50912e95365c7de
miral_kashiwagi <miral@infoseek.jp>
2024-05-26 18:56:07 +0900

    update test

--- playwright.config.js
+++ playwright.config.js
@@ -22,6 +22,9 @@ const config = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
+    toHaveScreenshot: {
+      stylePath: './screenshot.css'
+    },
     timeout: 60 * 1000
   },
   /* Run tests in files in parallel */
@@ -29,10 +32,11 @@ const config = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
